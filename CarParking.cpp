8f6884bba0bb28d87f14b2e0e010316cc9c41151
Shashank2409 <shashankgupta588@gmail.com>
2020-10-02 13:53:50 +0530

    Park and slot_no_for_driver_age done

--- CarParking.cpp
+++ CarParking.cpp
@@ -24,7 +24,9 @@ class Car{
         }
 };
 
-unordered_map<string , Car>car_info;
+unordered_map<string , Car>regNo_Car;
+unordered_map<int , unordered_set<Car>>driverAge_Car;
+unordered_map<int , Car>slotNo_Car; 
 
 class CarParking{
     private:
@@ -35,27 +37,23 @@ class CarParking{
         CarParking(){
             this->no_of_slots = 0;
         }
-        CarParking(int no_of_slots){
+        string set_slots(int no_of_slots){
+            string response , no_of_slots_string;
+
             this->no_of_slots = no_of_slots;
             alloted_slots.push_back(false);
             for(int slot = 1; slot <= no_of_slots; slot++){
                 available_slots.push(slot);
                 alloted_slots.push_back(false);
             }
-        }
-        string set_slots(int no_of_slots){
-            string response;
-            CarParking(no_of_slots);
 
             stringstream int_to_string(no_of_slots);
-            string no_of_slots_string;
             int_to_string>>no_of_slots_string;
             response = "Created parking of " + no_of_slots_string + " slots\n";
 
-            return response;
-            
+            return response;            
         }
-        string Park(string car_no , int driver_age){
+        string park(string car_no , int driver_age){
             string response;
             if(priority_queue.size() == 0){
                 response = "No parking slot available\n";
@@ -66,14 +64,23 @@ class CarParking{
                 alloted_slots[alloted_slot_no] = true;
 
                 Car car = new Car(car_no , driver_age , alloted_slot_no);
-                car_info.insert({car_no , car});
-                
+                regNo_Car.insert({car_no , car});
+                driverAge_Car[driver_age].insert(car);
+                slotNo_Car.insert({alloted_slot_no , car});
+
                 stringstream string_to_int(car_no);
                 string car_no_string;
                 string_to_int>>car_no_string;
                 response = "Car with vehicle registration number \"" + car_no + "\" has been parked at slot number " + car_no_string + "\n";
             }
             return response;
+        }
+        vector<int> slot_no_for_driver_age(int driver_age){
+            vector<int>ages;
+            for(auto it = driverAge_Car.begin(); it != driverAge_Car.end(); it++){
+                ages.push_back((it->second)->parking_slot_no);
+            }
+            return ages;
         }
 
 
@@ -84,12 +91,14 @@ int main(){
     ofstream output(output_file_path.c_str()); 
 
     string input_line;
+    CarParking parking_area;
+
     while(getline(in , input_line)){
         cout<<input_line<<endl;
         stringstream words(input_line);
         string command;
         words>>command;
-        CarParking parking_area;
+        
         if(command == "Create_parking_lot"){
             int no_of_slots;
             words>>no_of_slots;
@@ -100,7 +109,7 @@ int main(){
             int driver_age;
             words>>car_no>>useless_string>>driver_age;
             if(parking_area != NULL){
-                response = parking_area.Park(car_no , driver_age);
+                response = parking_area.park(car_no , driver_age);
             }
             else{
                 response = "We do not own a parking area yet\n";
@@ -110,10 +119,15 @@ int main(){
         else if(command == "Slot_numbers_for_driver_age"){
             int driver_age;
             words>>driver_age;
+            vector<int>ages = parking_area.slot_no_for_driver_age(driver_age);
+            for(int driver = 0; driver < ages.size(); driver++){
+                cout<<ages[driver]<<(driver == (ages.size() - 1)?"":",");
+            }
         }
         else if(command == "Slot_number_for_car_with_number"){
             string car_no;
             words>>car_no;
+            
         }
         else if(command == "Leave"){
             int slot_no;
